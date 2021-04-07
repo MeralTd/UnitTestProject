a2c4c20a2ac0253aca53df6e3826b36a8e9a2749
MeralTd <meraltasdemir77@gmail.com>
2021-04-07 23:39:42 +0300

    Create XUnit Test Project

---
+++ UdemyUnitTest.APP/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyUnitTest.APP
+{
+    public class Calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ UdemyUnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyUnitTest.APP;
+using Xunit;
+
+namespace UdemyUnitTest.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            //Arrange
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            //Act
+            var total = calculator.add(a, b);
+
+            //Assert
+            Assert.Equal<int>(25, total);
+        }
+    }
+}
